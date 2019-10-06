bdb4a298fc3eb0a6ee0bc937568afff3d0947fdd
c2909371614 <2909371614@qq.com>
2019-10-06 20:32:55 +0800

    matlab Gsuss Eli code

---
+++ Matrix/GaussEli.m
@@ -0,0 +1,23 @@
+function res = GaussEli(a)
+res = re_ech_form(a);
+Size = size(a);
+row = Size(1);
+column = Size(2);
+
+for i = 1:row
+    %--find the pivot position---
+    pivot = i;
+    while ~res(i, pivot) && pivot <= column
+        pivot = pivot + 1;
+    end
+    res(i,:) = res(i,:) / res(i, pivot);%let leading enty to zero
+    %---row operation----
+    for j =1 : row
+        if j == i
+            continue;
+        end
+        res(j,:) = res(j,:) - res(i,:) * res(j, pivot);
+    end
+end
+        
+    
